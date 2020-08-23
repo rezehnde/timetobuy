c20a79fcc246b002fcfe5cf5e1c3759269040a21
rezehnde <rezehnde@gmail.com>
2020-08-23 20:08:47 -0300

    HttpClient implemented

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,14 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class DashboardController extends AbstractController
 {
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
      * @Route("/dashboard")
      */
     public function page(): Response
     {
-        return $this->render('dashboard.html.twig');
+        $response = $this->client->request(
+            'GET',
+            'https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-06-30&base=EUR&symbols=USD'
+        );
+
+        $rates = [];
+        $statusCode = $response->getStatusCode();
+        $content = $response->toArray();
+
+        return $this->render('dashboard.html.twig', ['content' => $content]);
     }
 }
--- src/DataFixtures/GroupFixtures.php
+++ src/DataFixtures/GroupFixtures.php
@@ -11,18 +11,18 @@ class GroupFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $group = new Group();
-        $group->setName('Euro');
-        $group->setApiUrl('https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-06-30');
+        $group->setName('Euro x U.S. Dollar');
+        $group->setApiUrl('https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-06-30&base=EUR&symbols=USD');
         $manager->persist($group);
 
         $group = new Group();
-        $group->setName('U.S. Dollar');
-        $group->setApiUrl('https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-06-30&base=USD');
+        $group->setName('U.S. Dollar x Euro');
+        $group->setApiUrl('https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-06-30&base=USD&symbols=EUR');
         $manager->persist($group);
 
         $group = new Group();
-        $group->setName('Japanese Yen');
-        $group->setApiUrl('https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-06-30&base=JPY');
+        $group->setName('Japanese Yen x Euro');
+        $group->setApiUrl('https://api.exchangeratesapi.io/history?start_at=2020-01-01&end_at=2020-06-30&base=JPY&symbols=EUR');
         $manager->persist($group);
 
         $manager->flush();
