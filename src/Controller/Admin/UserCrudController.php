451747d631c79af8f45d305b398b8ccc3594aa73
rezehnde <rezehnde@gmail.com>
2020-08-23 11:10:54 -0300

    User CRUD configureFields improved

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +16,18 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            EmailField::new('email'),
+            TextField::new('password')
+                ->setFormType(PasswordType::class)
+                ->setFormTypeOption('always_empty', false)
+                ->hideOnIndex(),
+            ChoiceField::new('roles')
+                ->setChoices(['ROLE_ADMIN' => 'ROLE_ADMIN', 'ROLE_USER' => 'ROLE_USER'])
+                ->allowMultipleChoices(),
         ];
     }
-    */
 }
