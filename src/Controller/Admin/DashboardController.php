841e37235333c54aeb33e72ff91838973bcd2836
rezehnde <rezehnde@gmail.com>
2020-08-23 09:09:09 -0300

    CRUD User implemented

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -34,13 +35,23 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Timetobuy')
+            ->setTitle('Time to Buy')
+            ->setFaviconPath('images/favicons/favicon.ico')
         ;
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+
+            //MenuItem::section('Blog'),
+            //MenuItem::linkToCrud('Categories', 'fa fa-tags', Category::class),
+            //MenuItem::linkToCrud('Blog Posts', 'fa fa-file-text', BlogPost::class),
+
+            //MenuItem::section('Users'),
+            //MenuItem::linkToCrud('Comments', 'fa fa-comment', Comment::class),
+            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
+        ];
     }
 }
