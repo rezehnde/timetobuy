40cdda034fd9055036e90205b85ea68a99c209fe
rezehnde <rezehnde@gmail.com>
2020-08-23 13:39:13 -0300

    Encoder password when saving on admin

---
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeforeEntityUpdatedEvent::class => ['encodePassword'],
+            BeforeEntityPersistedEvent::class => ['encodePassword'],
+        ];
+    }
+
+    public function encodePassword($event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof User)) {
+            return;
+        }
+        $entity->setPassword($this->passwordEncoder->encodePassword($entity, $entity->getPassword()));
+    }
+}
